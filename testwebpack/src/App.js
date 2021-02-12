25ed9a61831116aebae0d9a29e782767412935bf
ShadeOfDream <48497879+ShadeOfDream@users.noreply.github.com>
2021-02-13 01:33:46 +0300

    Update App.js

--- testwebpack/src/App.js
+++ testwebpack/src/App.js
@@ -1,1 +1,22 @@
+import React,{Component} from 'react';
+import {Link,Router} from 'react-router-dom';
+import Users from './containers/Users';
+import Pizza from './containers/Pizza';
+
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <div>
+                    <Link to ="/">Users</Link> | <Link to = "/pizza">Pizza</Link>
+                </div>
+                <div>
+                    <Route path="/" exact component={Users}/>
+                    <Route path="/pizza" component={}/>
+                </div>
+            </div>
+        )
+    }
+}
 
