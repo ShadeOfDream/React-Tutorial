e1e20155a6d30abe9cabf3d1cfcf6db926e26cd2
ShadeOfDream <48497879+ShadeOfDream@users.noreply.github.com>
2021-02-13 01:27:02 +0300

    Create index.js

---
+++ testwebpack/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+const App = (
+  <BrowserRouter>
+    <App>/
+  </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
